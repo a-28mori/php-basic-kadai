9fa9df44ca8be21232de66b9b6c416716db97f42
a-28mori <mori28nov@icloud.com>
2026-04-28 10:53:59 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編11章課題</title>
+</head>
+
+<body>
+   <p>
+       <?php
+        $personal_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+       //  配列$user_namesのインデックスと値を1つずつ順番に出力する
+        foreach ($personal_data as $index => $value) {
+            echo "{$index}：{$value}<br>";
+        }
+       ?>
+   </p>
+</body>
+</html>
